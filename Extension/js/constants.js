2fa6ac3c57d44a454958e7c02d89a976fe394bc8
Laurent <l.deplaen@gmail.com>
2016-05-11 11:14:31 +0200

    Remove old project

--- Extension/js/constants.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Laurent De Plaen on 28/11/13.
- */
-var safekeysWebsiteUrl = 'http://localhost/www.therunningbrains.com/';
-var safekeysSettingsUrl = 'http://localhost/www.therunningbrains.com/#/settings';
