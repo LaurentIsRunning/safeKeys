54fea8e21e8f806952c7e6672bee00b1376e380d
Laurent <l.deplaen@gmail.com>
2016-05-11 11:13:42 +0200

    Adding files to the new visual studio solution

---
+++ SafeKeys/app/js/constants.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Laurent De Plaen on 28/11/13.
+ */
+var safekeysWebsiteUrl = 'http://localhost/www.therunningbrains.com/';
+var safekeysSettingsUrl = 'http://localhost/www.therunningbrains.com/#/settings';
