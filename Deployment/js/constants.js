abd1bf571b08bb282f999d33a7c60348b5f08fa1
Laurent <l.deplaen@gmail.com>
2016-05-11 11:18:00 +0200

    Remove old deployment folder

--- Deployment/js/constants.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Laurent De Plaen on 28/11/13.
- */
-var safekeysWebsiteUrl = 'http://www.therunningbrains.com';
-var safekeysSettingsUrl = 'http://www.therunningbrains.com/#/settings';
